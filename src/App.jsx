b5230623819d59f747745da5cfc7742ddaced70c
giacomotolari <tolarigiacomo@gmail.com>
2023-05-09 12:09:48 +0200

    fetch users from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
 import "./styles/app.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <p>test</p>
-    </div>
-  );
+  const API_URL = "https://jsonplaceholder.typicode.com/users";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(API_URL);
+      const data = await response.json();
+      console.log(data);
+    };
+    fetchData();
+  }, []);
+
+  return <div className="App"></div>;
 }
 
 export default App;
