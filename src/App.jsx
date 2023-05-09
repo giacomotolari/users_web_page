af992a5864ed0e7aca31399b8abdfb5edcd71924
giacomotolari <tolarigiacomo@gmail.com>
2023-05-09 13:19:03 +0200

    add initial state logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import "./styles/app.scss";
 
 function App() {
+  const [users, setUsers] = useState(() => {
+    const storedData = localStorage.getItem("users");
+    return storedData ? JSON.parse(storedData) : [];
+  });
+  console.log('users:',users);
   const API_URL = "https://jsonplaceholder.typicode.com/users";
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(API_URL);
       const data = await response.json();
-      console.log(data);
       localStorage.setItem("users", JSON.stringify(data));
     };
     fetchData();
