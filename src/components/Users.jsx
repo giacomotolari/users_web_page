9526aa8c1847b1ffee8d080044c6d81ed447447f
giacomotolari <tolarigiacomo@gmail.com>
2023-05-09 15:50:22 +0200

    solve task

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,10 +1,22 @@
 function Users({ users }) {
+  const handleMouseOver = (e) => {
+    e.currentTarget.style.borderColor = "red";
+  };
+
+  const handleMouseOut = (e) => {
+    e.currentTarget.style.borderColor = "";
+  };
+
+  // ref funktioniert nicht wenn the ref is an element of an array in a map
+
   return (
     <>
       {users.map((user) => (
         <div
           key={user.id}
           className="user-container"
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
         >
           <p>{user.name}</p>
           <p>{user.username}</p>
